fd9d44f3e5f1ed9c95a2d94e70bb8c69b8015b62
sirge <zakhar.timoshenko2004@gmail.com>
2021-04-17 19:33:09 +0300

    Добавлено множество багов и авторизация.

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,10 @@ DATABASES = {
     }
 }
 
-
+AUTGENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -116,6 +119,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_REDIRECT_URL = "/"
+
+EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
